59cd81d4be56ff44ee8a10955914870e1ac2af5a
Jacklyn Malone <jjmlmhc.09@gmail.com>
2021-10-22 18:09:56 -0400

    added questions array, working on function for writing the file

--- develop/index.js
+++ develop/index.js
@@ -1,13 +1,69 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+	type: 'input',
+	name: 'title',
+	message: 'What is your project title?',
+ },
+ {
+	type: 'input',
+	name: 'description',
+	message: 'What is your project about?',
+ },
+ {
+	type: 'input',
+	name: 'installation',
+	message: 'What are the installation instructions?',
+ },
+ {
+	type: 'input',
+	name: 'usage',
+	message: 'What is the usage information?',
+ },
+ {
+	type: 'input',
+	name: 'contributions',
+	message: 'What are the contribution guidelines?',
+ },
+ {
+	type: 'input',
+	name: 'test',
+	message: 'What are the test instructions?',
+},	
+{
+	type: 'list',
+	name: 'license',
+	message: 'Which license applies to your application?',
+	choices: ['GNU', 'Apache', 'MIT', 'ISC'],
+},
+{
+	type: 'input',
+	name: 'username',
+	message: 'What is your GitHub username?',
+},
+{
+	type: 'input',
+	name: 'email',
+	message: 'What is your email address?',
+},
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, answers) {
+// 	fs.writeFile(fileName, answers, (err) =>
+// 	err ? console.log(err) : console.log(answers))
+// }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+	inquirer.prompt(questions).then((answers) => {
+		console.log(answers);
+		// writeToFile();
+	})
+}
 
 // Function call to initialize app
 init();
